7962b7641e5e55e370789330e2969b59d1bfcef0
CreatureSurvive <cs@creaturecoding.com>
2018-07-28 14:16:39 -0500

    fetch preferences directly from usere preferences plist
    stupid NSUserDefaults

--- darkmail/darkmail.m
+++ darkmail/darkmail.m
@@ -1,6 +1,7 @@
 #include "darkmail.h"
 #include <spawn.h>
 #include <signal.h>
+#include <Preferences/PSSpecifier.h>
 
 @implementation DarkMailRootListController
 
@@ -37,6 +38,25 @@
 	pid_t pid;
 	const char* args[] = {"killall", "MobileMail", NULL};
 	posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
+}
+
+- (id)readPreferenceValue:(PSSpecifier*)specifier {
+	NSString *path = [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
+	NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+	[settings addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:path]];
+	return (settings[specifier.properties[@"key"]]) ?: specifier.properties[@"default"];
+}
+
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier*)specifier {
+	NSString *path = [NSString stringWithFormat:@"/User/Library/Preferences/%@.plist", specifier.properties[@"defaults"]];
+	NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+	[settings addEntriesFromDictionary:[NSDictionary dictionaryWithContentsOfFile:path]];
+	[settings setObject:value forKey:specifier.properties[@"key"]];
+	[settings writeToFile:path atomically:NO];
+	CFStringRef notificationName = (CFStringRef)specifier.properties[@"PostNotification"];
+	if (notificationName) {
+		CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), notificationName, NULL, NULL, YES);
+	}
 }
 
 @end
